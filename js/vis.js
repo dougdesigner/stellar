0f079c63b6ca1088ff3a1d3cbda21c7d5b9c1e21
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2023-11-26 23:36:03 -0800

    Alphabet to Google

--- js/vis.js
+++ js/vis.js
@@ -25,7 +25,7 @@ function createVis(data) {
     let sp500Data = [
         { Company: 'Apple', Percentage: '7.07%', MarketCap: '$2.950 trillion', ReturnYTD: '+22%' },
         { Company: 'Microsoft', Percentage: '6.74%', MarketCap: '$2.81 trillion', ReturnYTD: '+45%' },
-        { Company: 'Alphabet', Percentage: '4.08%', MarketCap: '$1.701 trillion', ReturnYTD: '+49%' },
+        { Company: 'Google', Percentage: '4.08%', MarketCap: '$1.701 trillion', ReturnYTD: '+49%' },
         { Company: 'Amazon', Percentage: '3.57%', MarketCap: '$1.49 trillion', ReturnYTD: '+58%' },
         { Company: 'NVIDIA', Percentage: '2.88%', MarketCap: '$1.20 trillion', ReturnYTD: '+241%' },
         { Company: 'Tesla', Percentage: '1.79%', MarketCap: '$744.82 billion', ReturnYTD: '+67%' },
