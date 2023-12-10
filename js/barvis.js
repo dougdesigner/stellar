94739053eb5ecf6cbeca5966588c80a90fcdfdfc
Douglas Sanchez <38049028+dougdesigner@users.noreply.github.com>
2023-12-10 14:55:31 -0800

    Update bars

--- js/barvis.js
+++ js/barvis.js
@@ -13,7 +13,7 @@ class BarVis {
         const element = document.getElementById(vis.parentElement);
         vis.margin = { top: 100, right: 40, bottom: 100, left: 100 };
         vis.width = element.offsetWidth - vis.margin.left - vis.margin.right;
-        vis.height = 500 - vis.margin.top - vis.margin.bottom;
+        vis.height = 400 - vis.margin.top - vis.margin.bottom;
 
         // Create SVG canvas
         vis.svg = d3.select("#" + vis.parentElement)
